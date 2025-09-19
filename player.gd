33e5c15bbf3f2d808d97986f1d87fdf423d9eb13
AmazinAxel <axel@amazinaxel.com>
2025-09-18 18:29:51 -0700

    feat: finish project!

---
+++ player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -800.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 12)
+
+	move_and_slide()
+
+
+func _on_win_bound_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://win.tscn")
+
+func _on_fail_bound_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://fail.tscn")
